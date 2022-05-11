f1d0ff0bc21c873ce82ef466282c8e127c3e1870
RhiH <rhi.herbert@gmail.com>
2022-05-11 13:43:26 +0000

    js test

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+//assemble memory game with section
+
+const section = document.querySelector('section');
+
+//generate data - note images are repeated 3 times as they will appear 3 times!
+
+const getData = () => [
+    {imgSrc:"../assets/images/Beryllium.jpg", name:"beryllium"},
+]
+
+console.log(getData);
+
