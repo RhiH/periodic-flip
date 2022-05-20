6b4934fee273b6443b9721ade32a3aa02a7af170
RhiH <rhi.herbert@gmail.com>
2022-05-20 18:01:13 +0000

    amend css & js for formatting

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,136 @@
 //assemble memory game with section
-
 const section = document.querySelector('section');
-// console.log(`SECTION: ${section}`);
-
 
 //generate data - note images are repeated 3 times as they will appear 3 times!
-
-const getData = () => [
-    {imgSrc:"../assets/images/beryllium2.jpg", name:"beryllium"},
-    {imgSrc:"../assets/images/boron2.jpg", name:"boron"},
-    {imgSrc:"../assets/images/carbon2.jpg", name:"carbon"},
-    {imgSrc:"../assets/images/fluorine2.jpg", name:"fluorine"},
-    {imgSrc:"../assets/images/helium2.jpg", name:"helium"},
-    {imgSrc:"../assets/images/hydrogen2.jpg", name:"hydrogen"},
-    {imgSrc:"../assets/images/lithium2.jpg", name:"lithium"},
-    {imgSrc:"../assets/images/neon2.jpg", name:"neon"},
-    {imgSrc:"../assets/images/nitrogen2.jpg", name:"nitrogen"},
-    {imgSrc:"../assets/images/oxygen2.jpg", name:"oxygen"},
-    {imgSrc:"../assets/images/beryllium2.jpg", name:"beryllium"},
-    {imgSrc:"../assets/images/boron2.jpg", name:"boron"},
-    {imgSrc:"../assets/images/carbon2.jpg", name:"carbon"},
-    {imgSrc:"../assets/images/fluorine2.jpg", name:"fluorine"},
-    {imgSrc:"../assets/images/helium2.jpg", name:"helium"},
-    {imgSrc:"../assets/images/hydrogen2.jpg", name:"hydrogen"},
-    {imgSrc:"../assets/images/lithium2.jpg", name:"lithium"},
-    {imgSrc:"../assets/images/neon2.jpg", name:"neon"},
-    {imgSrc:"../assets/images/nitrogen2.jpg", name:"nitrogen"},
-    {imgSrc:"../assets/images/oxygen2.jpg", name:"oxygen"},
-    {imgSrc:"../assets/images/beryllium2.jpg", name:"beryllium"},
-    {imgSrc:"../assets/images/boron2.jpg", name:"boron"},
-    {imgSrc:"../assets/images/carbon2.jpg", name:"carbon"},
-    {imgSrc:"../assets/images/fluorine2.jpg", name:"fluorine"},
-    {imgSrc:"../assets/images/helium2.jpg", name:"helium"},
-    {imgSrc:"../assets/images/hydrogen2.jpg", name:"hydrogen"},
-    {imgSrc:"../assets/images/lithium2.jpg", name:"lithium"},
-    {imgSrc:"../assets/images/neon2.jpg", name:"neon"},
-    {imgSrc:"../assets/images/nitrogen2.jpg", name:"nitrogen"},
-    {imgSrc:"../assets/images/oxygen2.jpg", name:"oxygen"},
+const getData = () => [{
+        imgSrc: "../assets/images/beryllium2.jpg",
+        name: "beryllium"
+    },
+    {
+        imgSrc: "../assets/images/boron2.jpg",
+        name: "boron"
+    },
+    {
+        imgSrc: "../assets/images/carbon2.jpg",
+        name: "carbon"
+    },
+    {
+        imgSrc: "../assets/images/fluorine2.jpg",
+        name: "fluorine"
+    },
+    {
+        imgSrc: "../assets/images/helium2.jpg",
+        name: "helium"
+    },
+    {
+        imgSrc: "../assets/images/hydrogen2.jpg",
+        name: "hydrogen"
+    },
+    {
+        imgSrc: "../assets/images/lithium2.jpg",
+        name: "lithium"
+    },
+    {
+        imgSrc: "../assets/images/neon2.jpg",
+        name: "neon"
+    },
+    {
+        imgSrc: "../assets/images/nitrogen2.jpg",
+        name: "nitrogen"
+    },
+    {
+        imgSrc: "../assets/images/oxygen2.jpg",
+        name: "oxygen"
+    },
+    {
+        imgSrc: "../assets/images/beryllium2.jpg",
+        name: "beryllium"
+    },
+    {
+        imgSrc: "../assets/images/boron2.jpg",
+        name: "boron"
+    },
+    {
+        imgSrc: "../assets/images/carbon2.jpg",
+        name: "carbon"
+    },
+    {
+        imgSrc: "../assets/images/fluorine2.jpg",
+        name: "fluorine"
+    },
+    {
+        imgSrc: "../assets/images/helium2.jpg",
+        name: "helium"
+    },
+    {
+        imgSrc: "../assets/images/hydrogen2.jpg",
+        name: "hydrogen"
+    },
+    {
+        imgSrc: "../assets/images/lithium2.jpg",
+        name: "lithium"
+    },
+    {
+        imgSrc: "../assets/images/neon2.jpg",
+        name: "neon"
+    },
+    {
+        imgSrc: "../assets/images/nitrogen2.jpg",
+        name: "nitrogen"
+    },
+    {
+        imgSrc: "../assets/images/oxygen2.jpg",
+        name: "oxygen"
+    },
+    {
+        imgSrc: "../assets/images/beryllium2.jpg",
+        name: "beryllium"
+    },
+    {
+        imgSrc: "../assets/images/boron2.jpg",
+        name: "boron"
+    },
+    {
+        imgSrc: "../assets/images/carbon2.jpg",
+        name: "carbon"
+    },
+    {
+        imgSrc: "../assets/images/fluorine2.jpg",
+        name: "fluorine"
+    },
+    {
+        imgSrc: "../assets/images/helium2.jpg",
+        name: "helium"
+    },
+    {
+        imgSrc: "../assets/images/hydrogen2.jpg",
+        name: "hydrogen"
+    },
+    {
+        imgSrc: "../assets/images/lithium2.jpg",
+        name: "lithium"
+    },
+    {
+        imgSrc: "../assets/images/neon2.jpg",
+        name: "neon"
+    },
+    {
+        imgSrc: "../assets/images/nitrogen2.jpg",
+        name: "nitrogen"
+    },
+    {
+        imgSrc: "../assets/images/oxygen2.jpg",
+        name: "oxygen"
+    },
 ];
 
 //randomize cards 
 
 const randomize = () => {
     const cardData = getData();
-    cardData.sort(() => Math.random() -0.5);
-    return(cardData);
-}; 
+    cardData.sort(() => Math.random() - 0.5);
+    return (cardData);
+};
 
 //create the cards
 
@@ -90,78 +175,68 @@ const checkCards = (e) => {
     const toggleCard = document.querySelectorAll('.toggleCard');
     console.log(flippedCards);
 
-//logic to create checker 
-    
-    if(flippedCards.length === 3) {
-        if(
+    //logic to create checker 
+    if (flippedCards.length === 3) {
+        if (
             flippedCards[0].getAttribute('name') ===
             flippedCards[1].getAttribute('name') &&
             flippedCards[1].getAttribute('name') ===
             flippedCards[2].getAttribute('name')
-        ){
+        ) {
             console.log("match");
 
-//change cards back to blank after flip and freeze correct match
-            flippedCards.forEach((card) =>{
+            //change cards back to blank after flip and freeze correct match
+            flippedCards.forEach((card) => {
                 card.classList.remove('flipped');
                 card.style.pointerEvents = 'none';
-        });
-        }
-        else {
+            });
+        } else {
             console.log("wrong");
 
-//change cards back to blank after flip inc. time limit.
-            flippedCards.forEach((card) =>{
+            //change cards back to blank after flip inc. time limit.
+            flippedCards.forEach((card) => {
                 card.classList.remove('flipped');
-                setTimeout(()=> card.classList.remove('toggleCard'), 1000);
+                setTimeout(() => card.classList.remove('toggleCard'), 1000);
             });
         }
     }
 
-//check if game is won
-        if(toggleCard.length === 30){
-            Swal.fire({
-                title: 'Congratulations!',
-                text: "You've won!",
-                confirmButtonColor: '#3085d6',
-                confirmButtonText: 'Yes! Play again'
-              }).then((result) => {
-                if (result.isConfirmed) {
-
-                }
-              });
-            restart('you won!');
-        }
-    };       
+    //check if game is won and congratulate winner
+    if (toggleCard.length === 30) {
+        Swal.fire({
+            title: 'Congratulations!',
+            text: "You've won!",
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'Yes! Play again'
+        }).then((result) => {
+            if (result.isConfirmed) {}
+        });
+        restart('you won!');
+    }
+};
 
 //restart game
+const restart = (text) => {
+    let cardData = randomize();
+    let faces = document.querySelectorAll('.front');
+    let cards = document.querySelectorAll('.card');
 
-    const restart = (text) => {
-        let cardData = randomize();
-        let faces = document.querySelectorAll('.front');
-        let cards = document.querySelectorAll('.card');
+    //make nothing clickable until game re-sets
+    section.style.pointerEvents = 'none';
+    cardData.forEach((item, index) => {
+        cards[index].classList.remove('toggleCard');
 
-//make nothing clickable until game re-sets
-
-        section.style.pointerEvents = 'none';
-        cardData.forEach((item,index) => {
-            cards[index].classList.remove('toggleCard');
-
- //re-randomize cards - pointer reset, images & names reset and delay timer
-//so you can't see the new image beneath. Add back click - pointerEvents
-            setTimeout(() => {
+        //re-randomize cards - pointer reset, images & names reset and delay timer
+        //so you can't see the new image beneath. Add back click - pointerEvents
+        setTimeout(() => {
             cards[index].style.pointerEvents = 'all';
             faces[index].src = item.imgSrc;
             cards[index].setAttribute('name', item.name);
             section.style.pointerEvents = 'all';
-            },1000);
-        });
-    
+        }, 1000);
+    });
 };
 
 cardCreator();
 
-// attempted fix Gemma_CI
-
-// if (typeof module !== "undefined") module.exports = { appendCard };
-module.exports = getData, randomize,  cardData, cardCreator;
+module.exports = getData, randomize, cardData, cardCreator;
